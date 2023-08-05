bb1890175f0ba2444e5cd1c4cfebb911eb4f4e4a
vitaliy <vitaliy@heliotrope.digital>
2023-08-05 11:21:12 +0300

    fetch and save by zip code

--- functions/index.js
+++ functions/index.js
@@ -5,17 +5,17 @@ const { logger } = require("firebase-functions/v2");
 
 admin.initializeApp();
 
+const zipcode = "19118";
+
 const fetchData = async () => {
   try {
     const response = await fetch(
-      "https://phl.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20current_date%20-%201%20AND%20zip%20LIKE%20%2719118%25%27%20"
+      `https://phl.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20current_date%20-%201%20AND%20zip%20LIKE%20%27${zipcode}%25%27%20`
     );
     const data = await response.json();
 
     const docRef = admin.firestore().collection("permits");
-    await Promise.all(
-      data.rows.map((post) => docRef.doc(post.zipcode.toString()).set(post))
-    );
+    await docRef.doc(zipcode).set(data);
 
     logger.log("Data fetch and save to Firestore completed");
   } catch (error) {
