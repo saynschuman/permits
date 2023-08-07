f6772f67038090681bbad5d2ab86830613e566ff
vitaliy <vitaliy@heliotrope.digital>
2023-08-07 11:39:36 +0300

    working added

--- functions/index.js
+++ functions/index.js
@@ -3,53 +3,51 @@ const admin = require("firebase-admin");
 const fetch = require("node-fetch");
 
 const zipCodes = [
+  "19116",
+  "19154",
+  "19115",
+  "19114",
+  "19152",
+  "19136",
+  "19111",
+  "19149",
+  "19135",
+  "19137",
+  "19124",
+  "19120",
+  "19126",
+  "19138",
+  "19150",
+  "19134",
+  "19140",
+  "19141",
+  "19119",
+  "19144",
+  "19118",
+  "19128",
+  "19133",
+  "19125",
+  "19122",
+  "19132",
+  "19129",
+  "19127",
+  "19123",
+  "19107",
+  "19106",
   "19102",
   "19103",
   "19104",
-  "19106",
-  "19107",
-  "19109",
-  "19113",
-  "19114",
-  "19115",
-  "19116",
-  "19118",
-  "19119",
-  "19120",
-  "19121",
-  "19122",
-  "19123",
-  "19124",
-  "19125",
-  "19126",
-  "19127",
-  "19128",
-  "19129",
-  "19130",
   "19131",
-  "19132",
-  "19133",
-  "19134",
-  "19135",
-  "19136",
-  "19137",
-  "19138",
+  "19151",
   "19139",
-  "19140",
-  "19141",
-  "19142",
   "19143",
-  "19144",
-  "19145",
   "19146",
   "19147",
   "19148",
-  "19149",
-  "19150",
-  "19151",
-  "19152",
-  "19153",
-  "19154",
+  "19112",
+  "19145",
+  "19142",
+  "19153"
 ];
 
 // Initialize the Firebase app
@@ -63,21 +61,22 @@ const fetchData = async () => {
       const response = await fetch(url);
       const data = await response.json();
 
-      const docRef = admin.firestore().collection("permits");
+      const chunkSize = 500;
+      for (let i = 0; i < data.rows.length; i += chunkSize) {
+        const batch = admin.firestore().batch();
 
-      // Transform the rows array into an object with the objectid as the key
-      const transformedRows = data.rows.reduce((acc, cur) => {
-        acc[cur.cartodb_id] = cur;
-        return acc;
-      }, {});
+        const chunk = data.rows.slice(i, i + chunkSize);
 
-      // Save the transformed data to Firestore
-      await docRef.doc(zipcode).set({
-        rows: transformedRows,
-        time: data.time,
-        fields: data.fields,
-        total_rows: data.total_rows,
-      });
+        chunk.forEach((row) => {
+          const docRef = admin
+            .firestore()
+            .collection(zipcode)
+            .doc(row.cartodb_id.toString());
+          batch.set(docRef, row);
+        });
+
+        await batch.commit();
+      }
 
       logger.log(
         `Data fetch and save to Firestore completed for zipcode: ${zipcode}`
