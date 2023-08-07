7e0fef04c2e56c03a84b4f09305f99673dad1545
vitaliy <vitaliy@heliotrope.digital>
2023-08-07 09:25:47 +0300

    listeners

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,96 @@
 const { scheduler, logger } = require("firebase-functions/v2");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
-const zipCodes = require("./codes");
+const { Transform } = require("stream");
+
+const zipCodes = [
+  "19102",
+  "19103",
+  "19104",
+  "19106",
+  "19107",
+  "19109",
+  "19113",
+  "19114",
+  "19115",
+  "19116",
+  "19118",
+  "19119",
+  "19120",
+  "19121",
+  "19122",
+  "19123",
+  "19124",
+  "19125",
+  "19126",
+  "19127",
+  "19128",
+  "19129",
+  "19130",
+  "19131",
+  "19132",
+  "19133",
+  "19134",
+  "19135",
+  "19136",
+  "19137",
+  "19138",
+  "19139",
+  "19140",
+  "19141",
+  "19142",
+  "19143",
+  "19144",
+  "19145",
+  "19146",
+  "19147",
+  "19148",
+  "19149",
+  "19150",
+  "19151",
+  "19152",
+  "19153",
+  "19154",
+];
 
 // Initialize the Firebase app
 admin.initializeApp();
 
-// Define an async function to fetch data for each zipcode
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    const data = JSON.parse(chunk.toString());
+
+    // Преобразовываем строки в объекты
+    const transformedRows = data.rows.reduce((acc, cur) => {
+      acc[cur.cartodb_id] = cur;
+      return acc;
+    }, {});
+
+    this.push(JSON.stringify(transformedRows));
+    callback();
+  },
+});
+
 const fetchData = async () => {
-  // Iterate over each zipcode
   for (let zipcode of zipCodes) {
     try {
-      // Make the request URL dynamic using template literals
-      const url = `https://phl.carto.com/api/v2/sql?q=SELECT * FROM permits WHERE permitissuedate >= current_date - 1 AND zip LIKE '${zipcode}%'`;
+      const url = `https://phl.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20permits%20WHERE%20zip%20LIKE%20%27${zipcode}%25%27%20`;
 
-      // Fetch data from the API
       const response = await fetch(url);
-      const data = await response.json();
 
-      // Get a reference to the Firestore collection "permits"
-      const docRef = admin.firestore().collection("permits");
+      const docRef = admin.firestore().collection("permits").doc(zipcode);
 
-      // Save the fetched data to the Firestore collection
-      await docRef.doc(zipcode).set(data);
+      response.body.pipe(transformStream).on("data", async (chunk) => {
+        await docRef.set({
+          rows: JSON.parse(chunk.toString()),
+          time: new Date().toISOString(), // обновляем время на текущее
+        });
+      });
 
-      // Log success message
       logger.log(
         `Data fetch and save to Firestore completed for zipcode: ${zipcode}`
       );
     } catch (error) {
-      // Log any error that occurs
       logger.error("An error occurred: ", error);
     }
   }
