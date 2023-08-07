9abe12d3d8ffed21647774a54cd0655bcdd28478
vitaliy <vitaliy@heliotrope.digital>
2023-08-07 08:34:59 +0300

    func fixed

--- functions/index.js
+++ functions/index.js
@@ -1,79 +1,39 @@
-const { scheduler } = require("firebase-functions/v2");
+const { scheduler, logger } = require("firebase-functions/v2");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
-const { logger } = require("firebase-functions/v2");
+const zipCodes = require("./codes");
 
+// Initialize the Firebase app
 admin.initializeApp();
 
-const zipCodes = [
-  "19102",
-  "19103",
-  "19104",
-  "19106",
-  "19107",
-  "19109",
-  "19113",
-  "19114",
-  "19115",
-  "19116",
-  "19118",
-  "19119",
-  "19120",
-  "19121",
-  "19122",
-  "19123",
-  "19124",
-  "19125",
-  "19126",
-  "19127",
-  "19128",
-  "19129",
-  "19130",
-  "19131",
-  "19132",
-  "19133",
-  "19134",
-  "19135",
-  "19136",
-  "19137",
-  "19138",
-  "19139",
-  "19140",
-  "19141",
-  "19142",
-  "19143",
-  "19144",
-  "19145",
-  "19146",
-  "19147",
-  "19148",
-  "19149",
-  "19150",
-  "19151",
-  "19152",
-  "19153",
-  "19154",
-];
-
+// Define an async function to fetch data for each zipcode
 const fetchData = async () => {
-  for (let i = 0; i < zipCodes.length; i++) {
-    const zipcode = zipCodes[i];
+  // Iterate over each zipcode
+  for (let zipcode of zipCodes) {
     try {
-      const response = await fetch(
-        `https://phl.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20permits%20WHERE%20zip%20LIKE%20%27${zipcode}%25%27%20`
-      );
+      // Make the request URL dynamic using template literals
+      const url = `https://phl.carto.com/api/v2/sql?q=SELECT * FROM permits WHERE permitissuedate >= current_date - 1 AND zip LIKE '${zipcode}%'`;
+
+      // Fetch data from the API
+      const response = await fetch(url);
       const data = await response.json();
 
+      // Get a reference to the Firestore collection "permits"
       const docRef = admin.firestore().collection("permits");
+
+      // Save the fetched data to the Firestore collection
       await docRef.doc(zipcode).set(data);
 
+      // Log success message
       logger.log(
         `Data fetch and save to Firestore completed for zipcode: ${zipcode}`
       );
     } catch (error) {
+      // Log any error that occurs
       logger.error("An error occurred: ", error);
     }
   }
 };
 
+// Export a Cloud Function that runs on a schedule
 exports.fetchPermits = scheduler.onSchedule("0 0 * * *", fetchData);
